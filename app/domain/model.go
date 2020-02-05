7957e92acc1343f7e3d749f8b0ca88f7e73694ac
Alexsandre Almeida <aralmeida@mpes.mp.br>
2020-02-05 18:30:43 -0300

    Change domain model for english

--- app/domain/model.go
+++ app/domain/model.go
@@ -2,22 +2,21 @@ package domain
 
 import "time"
 
-type Placar struct {
-	GolsTimeDaCasa    int `json:"golsTimeDaCasa"`
-	GolsTimeVisitante int `json:"golsTimeVisitante"`
+type Score struct {
+	HomeTeam int `json:"homeTeam"`
+	AwayTeam int `json:"awayTeam"`
 }
 
-type Jogo struct {
-	ID         int       `json:"id"`
-	Data       time.Time `json:"data"`
-	Local      string    `json:"local"`
-	TimeDaCasa string    `json:"timeDaCasa"`
-	Visitante  string    `json:"timeVisitante"`
-	Placar     Placar    `json:"placar"`
+type Match struct {
+	ID       int       `json:"id"`
+	Date     time.Time `json:"date"`
+	HomeTeam string    `json:"homeTeam"`
+	AwayTeam string    `json:"awayTeam"`
+	Score    Score     `json:"score"`
 }
 
-type Aposta struct {
-	ID     int    `json:"id"`
-	IDJogo int    `json:"idJogo"`
-	Placar Placar `json:"placar"`
+type Bet struct {
+	ID      int   `json:"id"`
+	IDMatch int   `json:"idMatch"`
+	Score   Score `json:"score"`
 }
